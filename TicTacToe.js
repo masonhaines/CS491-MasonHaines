addc84454f8fffb61292852a51b3b534b66cfeb3
masonhaines <levi.haines97@gmail.com>
2025-06-09 11:47:42 -0700

    made adjustments to meet assignment requirements
    changed colors of X's and O's
    changed colors of winning cells text content
    created a new file to place old running code and created a new class for active js file
    started jsDoc comments

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,211 +1,270 @@
-const cells = document.querySelectorAll(".cell"); // query all cells on the tic tac toe board
-const statusText = document.querySelector("#statusText");  // query select the h3 header with id statusText
-const clearButton = document.querySelector("#clearButton"); // query select the button with id clearButton
-const startButton = document.querySelector("#startButton"); // query select the button with id startButton
+/*Mason Haines 6/6/2025 */
 
-const winConditions = [
+class TicTacToe {
 
-    [0,1,2], 
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+    /**
+     * @param{number[][]} winConditions - 2d array of possible win conditions with in the gam e
+     * @param{string[]} options - array that holds the current state of the array 
+     * @param{string} currentPlayer - the current players character will be X or O
+     * @param{boolean} running - the current state of the game, whether it is running or not 
+     */
 
-];
+    constructor(options, currentPlayer, running, winConditions, cells, statusText, clearButton, startButton){
+        this.options = options;
+        this.currentPlayer = currentPlayer;
+        this.running = running;
+        this.winConditions = winConditions;
 
-let options = ["", "", "", "", "", "", "", "", "" ];
-let currentPlayer = "X";
-let running = false;
+        this.cells = cells;
+        this.statusText = statusText;
+        this.startButton = startButton;
+        this.clearButton = clearButton;
+    }
+    /**
+     * function that initializes the board game, inits the win conditions as well as the active places that are held on the board with options array 
+     * inits the DOM objects and queries them from the html doc 
+     * and inits the game to now be running and the starting player to be X
+     */
 
-startGame();
+    createBoard() {
+        const cells = document.querySelectorAll(".cell"); // query all cells on the tic tac toe board
+        const statusText = document.querySelector("#statusText");  // query select the h3 header with id statusText
+        const clearButton = document.querySelector("#clearButton"); // query select the button with id clearButton
+        const startButton = document.querySelector("#startButton"); // query select the button with id startButton
 
-function initGame() {
+        const winConditions = [
 
-    initCells();
-    // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
-    clearButton.addEventListener("click", restartGame);
-    statusText.textContent = `${currentPlayer}'s turn`;
-    running = true;
+            [0,1,2], 
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
 
-    startButton.removeEventListener("click", initGame);
+        ];
 
-}
+        const options = ["", "", "", "", "", "", "", "", "" ];
+        const currentPlayer = "X";
+        const running = false;
 
-// helper function to re init the cells object
-function initCells() {
-    cells.forEach(cell => cell.addEventListener("click", cellClicked)); // addEventListener(type, listener) 
-}
-
-function cellClicked() {
-
-    const cellIndex = this.getAttribute("cellIndex"); // this refers to what ever cell in clicked
-
-    if (options[cellIndex] != "" || !running ) {
-        return;
+        this.startGame();
     }
 
-    updateUsersChoiceCell(this, cellIndex);
-    checkWinner();
-}
+    /**
+     * prompts user to start a new game and initializes the start button to have an event listener for click
+     * on click calls initGame to make game playable 
+     */
 
-function updateUsersChoiceCell (cell, index) {
+    startGame () {
 
-    // options at the current index of cell clicked is being set to the current player either "X" or "O"
-    options[index] = currentPlayer;
-    cell.textContent = currentPlayer;
-    if (currentPlayer == "X") {
-        cell.style.color = "black";
+        statusText.textContent = `Press Start to Play!`;
+        startButton.addEventListener("click", initGame);
     }
-}
 
-function changePlayer() {
-    // if the current player is X we will assign the new current player to O
-    // currentPlayer = (currentPlayer == "X") ? currentPlayer = "O" : "X"; // old for testing 
-    if (currentPlayer === "X") {
+    /**
+     * makes game playable setting running to now true
+     * gives functionality to start a game and removes the event listener from the start button
+     * and creates and event listener for the clear button which calls restart game on click
+     */
 
-        currentPlayer = "O";
-        computerSelection();
+    initGame() {
 
-    } else {
+        running = true;
+        initCells();
+        this.enableClearButton();
+        this.displayCurrentPlayerForStatusText();
 
-        currentPlayer = "X";
-
+        startButton.removeEventListener("click", initGame);  // create event listener for start button to begin a new game 
     }
-    statusText.textContent = `${currentPlayer}'s turn`;
 
-}
+    /**
+     * display the current player whos turn it is to move 
+     * current player will display within the html element with statustext id 
+     */
 
-function checkWinner () {
-    let roundWon = false;
+    displayCurrentPlayerForStatusText() {
+        statusText.textContent = `${currentPlayer}'s turn`;
+    }
 
-    for(let i = 0; i < winConditions.length/*8*/; i++) {
+    /**
+     * adds event listener to clear button, is used if game is running 
+     * is called in init game and computer selection
+     */
 
-        // [0,1,2], 
-        // [3,4,5],
-        // [6,7,8],
-        // [0,3,6],
-        // [1,4,7],
-        // [2,5,8],
-        // [0,4,8],
-        // [2,4,6]
-        
-        // so if condition is winConditions at index [0] it would be [0,1,2]
-        // lets just say the options are all still empty " "
-        // that would mean the new 3 element array, conditions, is empty at all indexes
-        // that would mean that cellA cellb and cellC are empty as well because they are initialized to the value of the option array at ....
-        // the value of the 3 element condition array and in this examplew would be [0,1,2] but could be [0,4,8] etc
-
-        const condition = winConditions[i];
-
-        const cellA = options[condition[0]];
-        const cellB = options[condition[1]];
-        const cellC = options[condition[2]];
-
-        if (cellA == "" || cellB == "" || cellC == "") {
-
-            continue;
+    enableClearButton() {
+        if(running){
+            // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+            clearButton.addEventListener("click", restartGame);
         }
-        if (cellA == cellB && cellB == cellC) {
+  
+    }
 
-            roundWon = true;
-            cells[condition[0]].style.color = "greenyellow";
-            cells[condition[1]].style.color = "greenyellow";
-            cells[condition[2]].style.color = "greenyellow";
-            break;
+    /**
+     * helper function to re init the cells object adding the event listern for clicked 
+     * additonally resetting the color of the text content color of the cells 
+     */
+    initCells() {
+        cells.forEach(cell => cell.addEventListener("click", cellClicked)); // addEventListener(type, listener) 
+        cells.forEach(color => color.style.color= "black"); // reset color for all cell text content
+    }
+
+    cellClicked() {
+
+        const cellIndex = this.getAttribute("cellIndex"); // this refers to what ever cell in clicked
+
+        if (options[cellIndex] != "" || !running ) {
+            return;
+        }
+
+        updateUsersChoiceCell(this, cellIndex);
+        checkWinner();
+    }
+
+    updateUsersChoiceCell (cell, index) {
+
+        // options at the current index of cell clicked is being set to the current player either "X" or "O"
+        options[index] = currentPlayer;
+        cell.textContent = currentPlayer;
+        if (currentPlayer == "X") {
+            // cell.style.color = "black";
         }
     }
 
-    if (roundWon) {
+    changePlayer() {
+        // if the current player is X we will assign the new current player to O
+        // currentPlayer = (currentPlayer == "X") ? currentPlayer = "O" : "X"; // old for testing 
+        if (currentPlayer === "X") {
 
-        statusText.textContent = `${currentPlayer} wins! Press Clear to restart game`;
-        running = false;
-    }
-    else if (!options.includes("")) {
+            currentPlayer = "O";
+            computerSelection();
 
-        statusText.textContent = `Draw! Press Clear to restart game`;
-        running = false;
-    }
-    else {
-        changePlayer();
-    }
-}
+        } else {
 
-function restartGame () {
-    
-    currentPlayer = "X";
-
-    options = ["", "", "", "", "", "", "", "", ""];
-
-    statusText.textContent = `${currentPlayer}'s turn`;
-
-    cells.forEach(cell => cell.textContent = "");
-
-    running = false;
-    startGame();
-
-}
-
-function startGame () {
-
-    statusText.textContent = `Press Start to Play!`;
-    startButton.addEventListener("click", initGame);
-
-}
-
-async function computerSelection() {
-
-    cells.forEach(cell => cell.removeEventListener("click", cellClicked));
-
-    if (currentPlayer == "X") {
-        return;
-    }
-
-    var randomNumber = Math.floor(Math.random() * 9);
-    var randomOffset = Math.floor(Math.random() * 9);
-    let computerChoice = -1; 
-
-    // await sleep(2000); // dwell computer decision for x seconds
-    for (let i = 0; i < options.length; i ++) {
-
-        await sleep(300); 
-        if(i >=1) {
-            // cells[i-1].style.backgroundColor = "transparent";
-            recolorCells(i-1);
+            currentPlayer = "X";
         }
-        
-        if (options[i] == "") {
+        statusText.textContent = `${currentPlayer}'s turn`;
 
-            cells[i].style.backgroundColor = "greenyellow";
-            
-            if (randomNumber === i) {
-                computerChoice = i;
+    }
+
+    checkWinner () {
+        let roundWon = false;
+
+        for(let i = 0; i < winConditions.length/*8*/; i++) {
+
+            // so if condition is winConditions at index [0] it would be [0,1,2]
+            // lets just say the options are all still empty " "
+            // that would mean the new 3 element array, conditions, is empty at all indexes
+            // that would mean that cellA cellb and cellC are empty as well because they are initialized to the value of the option array at ....
+            // the value of the 3 element condition array and in this examplew would be [0,1,2] but could be [0,4,8] etc
+
+            const condition = winConditions[i];
+            const cellA = options[condition[0]];
+            const cellB = options[condition[1]];
+            const cellC = options[condition[2]];
+
+            if (cellA == "" || cellB == "" || cellC == "") {
+
+                continue;
+            }
+            if (cellA == cellB && cellB == cellC) {
+
+                roundWon = true;
+                cells[condition[0]].style.color = "red";
+                cells[condition[1]].style.color = "red";
+                cells[condition[2]].style.color = "red";
                 break;
             }
-            else if (randomNumber - i >= randomOffset && randomNumber - i >= computerChoice) {
-                computerChoice = i;
-            }
-            else {
-                computerChoice = i;
-            }
+        }
+
+        if (roundWon) {
+
+            statusText.textContent = `${currentPlayer} wins! Press Clear to restart game`;
+            running = false;
+        }
+        else if (!options.includes("")) {
+
+            statusText.textContent = `Draw! Press Clear to restart game`;
+            running = false;
+        }
+        else {
+            changePlayer();
         }
     }
-    
-    options[computerChoice] = currentPlayer;
-    cells[computerChoice].textContent = currentPlayer;
-    recolorCells(computerChoice);
-    cells[computerChoice].style.color = "#ffffff8f";
-    checkWinner();
-    initCells();
-}
 
-function sleep(ms) {
-    return new Promise(resolve =>setTimeout(resolve, ms)); // https://youtu.be/pw_abLxr4PI?si=Tlfw1HBU92o0wX3B
-}
+    restartGame () {
+        
+        currentPlayer = "X";
 
-function recolorCells(i) {
-    // cells.forEach(color => color.style.backgroundColor = "transparent"); // this works too but need to remove function param
-    cells[i].style.backgroundColor = "transparent";
-}
+        options = ["", "", "", "", "", "", "", "", ""];
+
+        statusText.textContent = `${currentPlayer}'s turn`;
+
+        cells.forEach(cell => cell.textContent = "");
+
+        running = false;
+        startGame();
+
+    }
+
+
+
+    async computerSelection() {
+
+        cells.forEach(cell => cell.removeEventListener("click", cellClicked));
+        clearButton.removeEventListener("click", restartGame);
+
+        if (currentPlayer == "X") {
+            return;
+        }
+
+        var randomNumber = Math.floor(Math.random() * 9);
+        var randomOffset = Math.floor(Math.random() * 9);
+        let computerChoice = -1; 
+
+        await sleep(300); // dwell computer decision for x seconds
+        for (let i = 0; i < options.length; i ++) {
+
+            await sleep(300); 
+            if(i >=1) {
+                // cells[i-1].style.backgroundColor = "transparent";
+                recolorCells(i-1);
+            }
+            
+            if (options[i] == "") {
+
+                cells[i].style.backgroundColor = "greenyellow";
+
+                if (randomNumber === i) {
+                    computerChoice = i;
+                    break;
+                }
+                else if (randomNumber - i >= randomOffset && randomNumber - i >= computerChoice) {
+                    computerChoice = i;
+                }
+                else {
+                    computerChoice = i;
+                }
+            }
+        }
+        
+        this.enableClearButton();// return event listern for clear button
+        options[computerChoice] = currentPlayer;
+        cells[computerChoice].textContent = currentPlayer;
+        recolorCells(computerChoice);
+        // cells[computerChoice].style.color = "#ffffff8f"; // this was to color the computers moves a different color for better differentiation
+        checkWinner();
+        initCells();
+    }
+
+    sleep(ms) {
+        return new Promise(resolve =>setTimeout(resolve, ms)); // https://youtu.be/pw_abLxr4PI?si=Tlfw1HBU92o0wX3B
+    }
+
+    recolorCells(i) {
+        // cells.forEach(color => color.style.backgroundColor = "transparent"); // this works too but need to remove function param
+        cells[i].style.backgroundColor = "transparent";
+    }
+
+};
